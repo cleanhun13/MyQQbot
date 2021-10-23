76ea8edf522bc20addadb30988809f4a8aa70323
cleanhun13 <cleanhun@outlook.com>
2021-10-23 21:54:42 +0800

    NLP

--- awesome/plugins/weather/__init__.py
+++ awesome/plugins/weather/__init__.py
@@ -1,4 +1,8 @@
 from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, IntentCommand
+from jieba import posseg
+
+from .data_source import get_weather_of_city
 
 
 # on_command 装饰器将函数声明为一个命令处理器
@@ -21,7 +25,24 @@ async def weather(session: CommandSession):
     await session.send(weather_report)
 
 
-async def get_weather_of_city(city: str) -> str:
-    # 这里简单返回一个字符串
-    # 实际应用中，这里应该调用返回真实数据的天气 API，并拼接成天气预报内容
-    return f'{city}的天气是……'
+# on_natural_language 装饰器将函数声明为一个自然语言处理器
+# keywords 表示需要响应的关键词，类型为任意可迭代对象，元素类型为 str
+# 如果不传入 keywords，则响应所有没有被当作命令处理的消息
+@on_natural_language(keywords={'天气'})
+async def _(session, NLPSession):
+    # 去掉首位空白字符
+    stripped_msg = session.msg_text.strip()
+    # 对消息进行分词和词性标注
+    words = posseg.lcut(stripped_msg)
+
+    city = None
+
+    # 遍历posseg.lcut返回的列表
+    for word in words:
+        # 每个元素是一个pair对象，包含word和flag两个属性，分别表示词和词性
+        if word.flag == 'ns':
+            # ns表示地名
+            city = word.word
+            break
+    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
+    return IntentCommand(90.0, 'weather', current_arg=city or '')
--- config.py
+++ config.py
@@ -6,3 +6,5 @@ COMMAND_START = {'/', '#'}
 
 HOST = '127.0.0.1'
 PORT = 8080
+
+NICKNAME = {'CC', 'cc', 'Cc', 'cC', '华子'}
